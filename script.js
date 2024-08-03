b99e6c22826bb985bf5e28e6e931cea2b998b7d5
epinox0022 <fjhonnymark@gmail.com>
2024-08-04 00:03:31 +0800

    Life

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-'use strict';
+"use strict";
 
-const hideMenu = document.querySelector('.menu');
-const openMenu = document.querySelector('.iconClick');
+const hideMenu = document.querySelector(".menu");
+const openMenu = document.querySelector(".iconClick");
+
+const openInfo = document.querySelector(".modalInfo");
+const clickInfo = document.querySelector(".iconClik");
 
 openMenu.onclick = () => {
-hideMenu.classList.toggle('menu');
-  }
+  hideMenu.classList.toggle("menu");
+};
 
-console.log(openMenu);  
+clickInfo.onclick = () => {
+  openInfo.classList.toggle("openInfo");
+};
+
+// console.log(openMenu);
