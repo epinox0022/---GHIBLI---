105d7767951e238013629a6e90e37304e60a59e2
epinox0022 <fjhonnymark@gmail.com>
2024-09-20 21:54:17 +0800

    mice

--- script.js
+++ script.js
@@ -7,3 +7,42 @@ function rover() {
 function closerover() {
   document.getElementById("headered").classList.remove("hover");
 }
+
+const textElement = document.getElementById("text");
+
+const startColor = {
+  r: 4,
+  g: 44,
+  b: 29,
+};
+
+const endColor = {
+  r: 244,
+  g: 253,
+  b: 255,
+};
+
+const maxScrollValue = 400;
+
+const updateTextColor = () => {
+  const scrollPosition = Math.min(window.scrollY, maxScrollValue);
+
+  const red = Math.round(
+    startColor.r +
+      (endColor.r - startColor.r) * (scrollPosition / maxScrollValue)
+  );
+  const green = Math.round(
+    startColor.g +
+      (endColor.g - startColor.g) * (scrollPosition / maxScrollValue)
+  );
+  const blue = Math.round(
+    startColor.b +
+      (endColor.b - startColor.b) * (scrollPosition / maxScrollValue)
+  );
+
+  // Set the new color based on the scroll position
+  textElement.style.color = `rgb(${red}, ${green}, ${blue})`;
+};
+
+// Add scroll event listener to trigger the color change
+window.addEventListener("scroll", updateTextColor);
